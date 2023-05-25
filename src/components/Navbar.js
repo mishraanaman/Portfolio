a949f6b7bafcd34a91edec03c4e3bf684fe6e48b
Naman <naman@192.168.1.3>
2023-05-26 02:01:12 +0530

    removed dep

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ import logo from "../Assets/Projects/favicon.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
+//import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
-  AiOutlineFundProjectionScreen,
+  //AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
 

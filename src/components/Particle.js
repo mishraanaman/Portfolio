f4785d4114139350b2c1ee22b995082b0c232080
Naman <naman@192.168.1.3>
2023-05-27 01:06:36 +0530

    particle and avatar change

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -6,47 +6,103 @@ function Particle() {
     <Particles
       id="tsparticles"
       params={{
+        name: "NASA",
         particles: {
           number: {
             value: 160,
             density: {
-              enable: true,
-              value_area: 1500,
-            },
+              enable: true
+            }
           },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
+          color: {
+            value: "#ffffff"
           },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
+          shape: {
+            type: "triangle"
           },
           opacity: {
-            anim: {
+            value: 0.75,
+            random: true,
+            animation: {
               enable: true,
               speed: 1,
-              opacity_min: 0.05,
-            },
+              opacity_min: 0,
+              sync: false
+            }
           },
+          size: {
+            value: 3,
+            random: true,
+            animation: {
+              enable: false,
+              speed: 4,
+              size_min: 0.3,
+              sync: false
+            }
+          },
+          links: {
+            enable: false,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            random: true,
+            straight: false,
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 600
+            }
+          }
         },
         interactivity: {
           events: {
-            onclick: {
+            onHover: {
               enable: true,
-              mode: "push",
+              mode: "bubble"
             },
+            onClick: {
+              enable: true,
+              mode: "repulse"
+            }
           },
           modes: {
-            push: {
-              particles_nb: 1,
+            grab: {
+              distance: 400,
+              links: {
+                opacity: 1
+              }
             },
-          },
+            bubble: {
+              distance: 250,
+              size: 0,
+              duration: 2,
+              opacity: 0
+            },
+            repulse: {
+              distance: 400,
+              duration: 0.4
+            },
+            push: {
+              quantity: 4
+            },
+            remove: {
+              quantity: 2
+            }
+          }
         },
-        retina_detect: true,
+        background: {
+          color: "##080707",
+          position: "50% 50%",
+          repeat: "no-repeat",
+          size: "20%"
+        }
       }}
     />
   );

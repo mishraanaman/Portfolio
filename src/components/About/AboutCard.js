64d8c34e6d10df9a6f873eaff8ae5f46cd88c274
unknown <reachoutnaman@gmail.com>
2023-10-25 00:42:53 +0530

    content changes on website

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -14,6 +14,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi everyone, I'm <span className="purple">Naman Mishra</span>
             . I have worked as a <span className="purple">Software developer </span>for the past 3 years after completing my graduation from <span className="purple">Harcourt Butler Technical University</span>.
+            As an alumni of <span className="purple">ZTM Academy</span> and <span className="purple">Namaste React</span> I try to contribute to <span className="purple">Open Source</span> projects frequently.
             <br />
             <br />
           </p>

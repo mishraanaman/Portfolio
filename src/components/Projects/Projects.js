3f62499d8c61ac703d0c12a1e61a08b97460a99e
Naman Mishra <reachoutnaman@gmail.com>
2025-10-24 09:47:50 +0530

    updated projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import LiquidEtherDefault from "../LiquidEtherDefault";
-import coconut from "../../Assets/Projects/coconut.png"
+import mmFlix from "../../Assets/Projects/mmflix.png"
 import Youthhub from "../../Assets/Projects/YouthHub.png"
 
 
@@ -15,29 +15,25 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={mmFlix}
+              isBlog={false}
+              title="mmFlix"
+              description=" Browse your favorite movies and TV shows. Web App built using Atlas Search, ReactJS and Express."
+              ghLink="https://github.com/mishraanaman/mongo-mflix"
+              demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Youthhub}
               isBlog={false}
               title="YouthHub"
               description="Search and Stream vidoes from the original library of Youtube. This web app is a Youtube clone built using ReactJs and Redux Toolkit."
-              ghLink="https://github.com/namanmish/Youthhub"
+              ghLink="https://github.com/mishraanaman/Youthhub"
               demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={coconut}
-              isBlog={false}
-              title="Coconut"
-              description=" Purchase your favorite ethnic attire for this season. Web App built using ReactJS and Redux Toolkit."
-              ghLink="https://github.com/namanmish/coconut-fashion"
-              demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 {/* 
